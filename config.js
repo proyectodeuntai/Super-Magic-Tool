b3fff14fcbd9b01ba12f234f13cf51f7dcc7cbd3
Dani <darkdexterminate@gmail.com>
2026-06-12 22:23:12 +0200

    Añadido config.js con reglas de seguridad

---
+++ config.js
@@ -0,0 +1,14 @@
+// ═══════════════════════════════════════════════════════════
+// config.js — NUNCA subas este archivo a GitHub
+// Añádelo a .gitignore
+// ═══════════════════════════════════════════════════════════
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDMSVL3E_YnyTL3YVgHpU9Hk5XGYwEU3Zw",
+  authDomain: "magiccolection-f6ef4.firebaseapp.com",
+  databaseURL: "https://magiccolection-f6ef4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "magiccolection-f6ef4",
+  storageBucket: "magiccolection-f6ef4.firebasestorage.app",
+  messagingSenderId: "598775830444",
+  appId: "1:598775830444:web:0b535ba09fd1c87b8d0c09"
+};
